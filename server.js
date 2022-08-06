cb5709f0c7525cf1043d7c1bdfb163893042dc2a
FFFF-0000h <adebiyiitunuayo@gmail.com>
2022-08-06 15:30:59 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mockUserData=[
+{name:'Mark'},
+{name:'Jill'}
+]
+app.get('/users', function(req,res){
+ 	res.json({
+ 	 	success: true,
+ 	 	message: 'successfully got users. Nice!',
+ 	 	users: mockUserData
+ 	})
+})
 app.listen(8000,function(){ 
 	console.log("server is running") 
 })
