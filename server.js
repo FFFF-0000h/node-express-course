10e75a8ff5286bde6a3fe9595f16da0249c1d6cb
FFFF-0000h <adebiyiitunuayo@gmail.com>
2022-08-06 16:21:00 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
@@ -21,5 +22,26 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login',function(req,res){
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+
+ 	if (username===mockUsername && password===mockPassword){
+      	res.json({
+      	 	success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+      	})
+ 	} else {
+      	res.json({
+      	 	success: false,
+      	 	message: 'password and username do not match'
+      	})
+ 	}
 })
 app.listen(8000,function(){console.log('server is listening')})
